78f92d7bc75729076ef205ffa779bd06f657d0cc
Byrone Songa <byronesonga68@gmail.com>
2024-10-20 23:14:19 +0300

    task6

---
+++ task_6.sql
@@ -0,0 +1,4 @@
+INSERT INTO customer ("customer_id", "customer_name", "email", "address")
+VALUES("Blessing Malik", "bmalik@sandtech.com", "124 Happiness  Ave.")
+("Obed Ehoneah", "eobed@sandtech.com", "125 Happiness  Ave.")
+("Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness  Ave.")
