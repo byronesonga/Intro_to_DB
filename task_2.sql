abfb01ad4da38a026d29aa10796b37cf82b36ead
Byrone Songa <byronesonga68@gmail.com>
2024-10-20 23:05:44 +0300

    task1

---
+++ task_2.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE IF NOT EXISTS alx_book_store
+
+use alx_book_store
+
+
+
+CREATE  TABLE Authors(
+author_id (Primary Key)
+author_name VARCHAR(215)
+)
+
+CREATE TABLE Books(
+book_id PRIMARY KEY
+title VARCHAR (130)
+author_id (Foreign Key referencing Authors table)
+price DOUBLE
+publication_date DATE
+)
+
+CREATE TABLE Customers(
+customer_id PRIMARY KEY
+customer_name VARCHAR(215)
+email VARCHAR(215)
+address TEXT
+)
+CREATE TABLE Orders(
+order_id INT PRIMARY KEY
+customer_id INT FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+order_date DATE
+)
+CREATE TABLE Order_Details(
+orderdetailid  PRIMARY Key
+order_id FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+book_id INT FOREIGN KEY (book_id) REFERENCES Books(book_id)
+quantity DOUBLE
+)
