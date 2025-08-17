de3c37854a73714f711bd008fcfec7693607c6b4
Byrone Songa <byronesonga68@gmail.com>
2025-08-17 07:41:36 +0300

    description

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,11 @@
-USE alx_book_store; 
-SHOW COLUMNS FROM books; 
+SELECT 
+    column_name        AS 'Column',
+    column_type        AS 'Type',
+    is_nullable        AS 'Null',
+    column_key         AS 'Key',
+    column_default     AS 'Default',
+    extra              AS 'Extra'
+FROM information_schema.columns
+WHERE table_schema = 'songa_database'
+  AND table_name   = 'books'
+ORDER BY ordinal_position;
